e3e627433cb74b968cba3286a57bc4fc40d1cba7
Mark Canning <argusdusty@gmail.com>
2012-12-05 23:40:34 -0800

    Fix typo

--- errorcorrect.go
+++ errorcorrect.go
@@ -34,7 +34,7 @@ var LowercaseASCII = []byte{
 // All lowercase ASCII (a-z)
 var LowercaseLetters = []byte{
 	97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110,
-	111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122
+	111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122,
 }
 
 // Returns all byte-arrays which are Levenshtein distance of 1 away from Word
