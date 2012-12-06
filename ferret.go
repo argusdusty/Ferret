89e8785841a348a6a18068f289278090a0f705f2
Mark Canning <argusdusty@gmail.com>
2012-12-05 23:59:26 -0800

    Fix LSIS algorithm

--- ferret.go
+++ ferret.go
@@ -243,6 +243,7 @@ func (LSIS LengthSortedInvertedSuffix) Query(Query string, ResultsLimit, LengthL
 	Results := make([]string, 0)
 	for i := n; i < LengthLimit; i++ {
 		if _, ok := LSIS[n]; !ok { continue }
+		IS := LSIS[n]
 		Results = append(Results, IS.Query(Query, ResultsLimit, true, false, make([]byte, 0))...)
 		n := len(Results)
 		if n == ResultsLimit { return Results }
